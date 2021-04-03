b67844e23e5d4fea2b6bdb0bdda1438ee18d62d5
WooDong <dwpark2007@naver.com>
2021-04-03 23:48:52 +0900

    MongoDB 연동 및 User Schema/API 함수 작성

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-const express = require('express'); 
-const app = express(); 
-const cors = require('cors'); 
 const bodyParser = require('body-parser'); 
-const port = process.env.PORT || 5000; 
+const express = require('express'); 
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const app = express(); 
+
+
+// Environment Setting ===========================
+const dotenv = require('dotenv'); 
+const path = require('path'); 
+
+dotenv.config({ 
+    path: path.resolve(
+        process.cwd(), 
+        ".env"
+    )
+})
+
+const { User } = require('./models/User'); 
+
 
 app.use(cors()); 
 
 app.use(bodyParser.json()); 
+
+// Test  ========================================
 app.get('/api/test', (req, res) => { 
     const test_data = {
         userName: 'Const-User1'
@@ -15,6 +32,113 @@ app.get('/api/test', (req, res) => {
     res.json(test_data); 
 })
 
+// MongoDB Setting ==============================
+const {
+    DB_ADDRESS, DB_PORT, 
+    DB_USER, DB_PASSWORD, DB_DATABASE
+} = process.env; 
+const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_ADDRESS}:${DB_PORT}`
+
+mongoose.connect(DB_URI, {
+    dbName: DB_DATABASE, 
+	useNewUrlParser: true, useUnifiedTopology: true,
+	useCreateIndex: true, useFindAndModify: true
+}).then( () => console.log("MongoDB Connected..")).catch(err => console.log(err)) 
+
+
+const auth = (req, res, next) => { 
+    // authentication 
+
+    // get token in clinet's cookie 
+    let token = req.cookies.x_auth; 
+
+    // decode token and find user 
+    User.findByToken(token, (err, user) => { 
+        if(err) throw err; 
+        if(!user) return res.json({ isAuth: false, error: true })
+        
+        req.token = token; 
+        req.user = user; 
+        next(); 
+    })
+    // if user exist, authentication complete
+}
+
+app.post('/api/users/signup', (req, res) => { 
+
+    // Sign up
+    const user = new User(req.body);
+
+    user.save((err, user) => { 
+        if (err) return res.json({ success: false, err })
+        
+        res.cookie("x_auth", user.token).status(200).json({ 
+            success: true, 
+            userId: user._id
+        })
+    })
+    return res.status(200).send({ 
+        message: "회원가입이 완료되었습니다!",
+        success: true
+    })
+})
+
+app.post('/api/users/signin', (req, res) => { 
+
+    // Log in
+    User.findOne({ email: req.body.email }, (err, user) => { 
+        if(!user) { 
+            return res.json({ 
+                success: false, 
+                message: "Not found: email address.."
+            })
+        }
+        
+        user.comparePassword(req.body.password, (err, isMatch) => { 
+            if(!isMatch) return res.json({
+                success: false, 
+                message: "Wrong Password"
+            })
+
+            // Generate token if password is correct 
+            user.generateToken( (err, user) => { 
+                if (err) return res.status(400).send(err); 
+
+                // Save token in Cookie
+                res.cookie("x_auth", user.token).status(200).json({ 
+                    success: true, 
+                    userId: user._id
+                })
+            }) 
+        })
+    })
+})
+
+
+app.get('/api/users/auth', auth, (req, res) => { 
+
+    res.status(200).json({ 
+        _id: req.user._id, 
+        // isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true, 
+        email: req.user.email, 
+        name: req.user.name 
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => { 
+
+    User.findOneAndUpdate({_id: req.user._id}, 
+        { token: "" }, 
+        (err, user) => { 
+            if(err) return res.json({ success: false, err}); 
+            return res.status(200).send({ 
+                success: true
+        })
+    })
+})
+
+const port = process.env.PORT || 5000; 
 app.listen(port, () => { 
-    console.log(`express is running on ${port}`); 
+    console.log(`express is running on ${process.env.port}`); 
 })
---
+++ server/models/User.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose'); 
+const bcrpyt = require('bcrypt'); 
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
+var Schema = mongoose.Schema; 
+
+var UserSchema = new Schema({ 
+    name: { 
+        type: String, 
+        maxlength: 50
+    }, 
+    email: { 
+        type: String, 
+        trim: true, 
+        unique: 1
+    }, 
+    password: { 
+        type: String, 
+        minlength: 5
+    }, 
+    token: { 
+        type: String
+    }
+});
+
+
+UserSchema.pre('save', function(next)  { 
+
+    var user = this; 
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token 
+    
+    if(user.isModified('password')) { 
+
+        // password Encrypting 
+        bcrpyt.genSalt(saltRounds, function (err, salt) { 
+            if(err) return next(err);
+
+            bcrpyt.hash(user.password, salt, function(err, hash) { 
+                if(err) return next(err);
+
+                user.password = hash;
+                next()
+            })
+        })
+    } else { 
+        next() 
+    }
+}) 
+
+UserSchema.methods.comparePassword = function(plainPassword, cb) { 
+    bcrpyt.compare(plainPassword, this.password, function(err, isMatch) { 
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+UserSchema.methods.generateToken = function(cb) { 
+
+    var user = this; 
+
+    // Generate Token via jsonwebtoken
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token 
+
+    user.save(function(err, user) { 
+        if(err) return cb(err) 
+        cb(null, user) 
+    })
+}
+
+
+UserSchema.statics.findByToken = function(token, cb) { 
+    var user = this; 
+    // decode token 
+    jwt.verify(token, 'secretToken', function(err, decoded) { 
+        // find user by user._id
+        // check if token is equal with clinet's token
+        user.findOne({"_id": decoded, "token": token}, function(err, user) { 
+            if(err) return cb(err); 
+            cb(null, user) 
+        })
+    }) 
+}
+
+const User = mongoose.model("User", UserSchema)
+module.exports = { User }
