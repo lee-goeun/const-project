f5ddaa0f0901d6060dcb49bc92ce86a92cae3bb8
Woo-Dong <dwpark2007@naver.com>
2021-06-28 08:25:42 +0900

    최대 너비 설정

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
         window.__REDUX_DEVTOOLS_EXTENSION__() 
         )} 
       >
-        <div className="App justify-content-center" style={{padding: '0', margin: '0 auto'}} >
+        <div className="App justify-content-center" style={{padding: '0', margin: '0 auto', maxWidth: '450px'}} >
           <Container fluid='md'>
             <Switch>
               <Route exact path="/" component={Auth(MainPage, false)} /> 
