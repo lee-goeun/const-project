1b2a9ebb03f8d0d7e314976e2a030172119ff981
woniyoon <gellifisch@gmail.com>
2021-07-29 22:57:21 +0900

    포트폴리오 메인, 아이콘 추가 및 스타일 수정

--- src/views/DashboardPage/PortfolioMain.js
+++ src/views/DashboardPage/PortfolioMain.js
@@ -6,7 +6,7 @@ import { IconButton } from "@material-ui/core";
 import Menu from '@material-ui/icons/Menu';
 import { Row, Col, Button } from 'react-bootstrap'; 
 import logo_img from "static/img/logo_img.png";
-
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function PortfolioMain(props) {
 
@@ -35,49 +35,62 @@ function PortfolioMain(props) {
       </Row>
       <Row>
         <Col align='center' style={{ marginTop: '20px' }}>
-          <div style={{ width: '90vw', backgroundColor: '#615EFF', borderRadius: '5px', boxShadow: '0px 0px 14px -1px rgba(0,0,0,0.4)' }}>
+          <div style={{ width: '90vw', backgroundColor: '#615EFF', borderRadius: '3px', boxShadow: '0px 0px 14px -1px rgba(0,0,0,0.4)' }}>
             <span 
               style={{ 
-                padding: '10px',
+                padding: '20px',
                 color: '#A2B9FC', 
                 display: 'flex', 
                 flexDirection: 'row', 
                 justifyContent: 'space-between',
               }}
             >
-              <p style={{ fontSize: '1em' }}>순자산</p>
-              <p style={{ color: '#F2F2F2', fontSize: '1.2em' }}>0원</p>
+              <p style={{ margin: 'auto 0', fontSize: '1em', alignSelf: 'center' }}>순자산</p>
+              <span style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center'}}>
+                <p style={{ margin: 'auto 0', color: '#F2F2F2', fontSize: '1.2em' }}>0원&nbsp;</p>
+                <NavigateNextIcon style={{alignSelf:'center'}} />
+              </span>
             </span>
             <span 
               style={{ 
-                padding: '10px',
+                padding: '20px',
                 color: '#A2B9FC', 
                 display: 'flex', 
                 flexDirection: 'row', 
                 justifyContent: 'space-between',
               }}
             >
-              <p style={{ fontSize: '1em' }}>총자산</p>
-              <p style={{ color: '#F2F2F2', fontSize: '1.2em' }}>0원</p>
+              <p style={{ margin: 'auto 0', fontSize: '1em', alignSelf: 'center' }}>총자산</p>
+              <span style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center'}}>
+                <p style={{ margin: 'auto 0', color: '#F2F2F2', fontSize: '1.2em' }}>0원&nbsp;</p>
+                <NavigateNextIcon style={{alignSelf:'center'}} />
+              </span>
             </span>
             <span 
               style={{ 
-                padding: '10px',
+                padding: '20px',
                 color: '#A2B9FC', 
                 display: 'flex', 
                 flexDirection: 'row', 
                 justifyContent: 'space-between',
               }}
             >
-              <p style={{ fontSize: '1em' }}>총부채</p>
-              <p style={{ color: '#F2F2F2', fontSize: '1.2em' }}>0원</p>
+              <p style={{ margin: 'auto 0', fontSize: '1em', alignSelf: 'center' }}>총부채</p>
+              <span style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center'}}>
+                <p style={{ margin: 'auto 0', color: '#F2F2F2', fontSize: '1.2em' }}>0원&nbsp;</p>
+                <NavigateNextIcon style={{alignSelf:'center'}} />
+              </span>
             </span>
           </div>
         </Col>
         <Col style={{ margin: '20px 0' }}>
           <h5>stacked bar chart 넣는 자리</h5>
         </Col>
+      </Row>
 
+      {/* 포트폴리오 메인 경계선 */}
+      <Row>
+        <div style={{ backgroundColor: '#F5F5F5', height: '10px', width: '100%' }}></div>
       </Row>
     </>
   )
